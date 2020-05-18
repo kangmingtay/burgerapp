b66885ef0f3235be8af67c49394eb9a95aa9c5fa
kangmingtay <kang_ming_tay@apple.com>
2020-05-18 17:53:59 +0800

    add logo for app

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import classes from './Toolbar.css'
+import Logo from '../../Logo/Logo';
+import classes from './Toolbar.css';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
-        <div>Logo</div>
+        <Logo/>
         <nav>
             ...
         </nav>
